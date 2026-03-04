46906c578d9743bb6087436d332db1394176cad4
Vinier09 <engemarc187@gmail.com>
2026-03-04 22:14:52 +0100

    Enemy_health

---
+++ Assets/Scripts/Enemy_logic.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Enemy_logic : MonoBehaviour
+{
+    int Health = 300;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
